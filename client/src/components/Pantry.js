f87445ce8936c7fe076a3fe965463ceb66c436fc
Adam Crespo <adam.crespo101@outlook.com>
2022-07-03 12:43:45 -0400

    started building models

--- client/src/components/Pantry.js
+++ client/src/components/Pantry.js
@@ -1,6 +1,16 @@
+
+
 function Pantry(){
+
     return(
-        <div>Pantry</div>
+        <div className="pantry">
+            <h1>This is your pantry, add ingredients to your pantry to return some recipes!</h1>
+            <form>
+                <input type="text" placeholder="add your ingredients here"/>
+                <button type="submit">Add</button>
+            </form>
+            <div className="ingredient-list"></div>
+        </div>
     )
 }
 
---
+++ db/migrate/20220703163718_create_recipes.rb
@@ -0,0 +1,9 @@
+class CreateRecipes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :recipes do |t|
+      t.string :instructions
+
+      t.timestamps
+    end
+  end
+end
