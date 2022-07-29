af647a3d9fecf7b6232391c48c59ccb13594569b
Adam Crespo <adam.crespo101@outlook.com>
2022-07-29 11:00:30 -0400

    more

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,25 @@ import Home from './components/Home';
 import PartyRoom from './components/PartyRoom';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import {useState, useEffect} from 'react'
-
-
+import $ from 'jquery'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import axios from 'axios'
 
 function App() {
 
-
+  const [token, setToken] = useState("")
+  const clientId = '5326b87d878e436d8c9c8c94722308fb'
+  const clientSecret = 'f51065a9f24246d0a7026ee4c89ee3f8'
+  const redirect = "http://localhost:4000"
+  function play (){
+    
+  }
   return (
     <div className="App">
       <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/PartyRoom" element={<PartyRoom />} />
+        <Route path="/PartyRoom" element={<PartyRoom axios={axios}/>} />
       </Routes>
       </BrowserRouter>
     </div>
--- client/src/components/PartyRoom.js
+++ client/src/components/PartyRoom.js
@@ -1,8 +1,10 @@
 import logo from '../VoxParty-logos.jpeg'
+import $ from 'jquery'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 
-function PartyRoom(){
-
+function PartyRoom({axios}){
+    console.log(axios)
     return(
         <>
         <h3 id='info-box'>Pass the aux back and forth between users! You may also return <a href="/">Home</a>!</h3>
@@ -10,7 +12,7 @@ function PartyRoom(){
             <img src={logo} alt="VoxParty logo" className='home-logo' style={{height: "30vh"}}/>
         </div>
         <div className='music-player'>
-            
+
         </div>
         </>
     )
